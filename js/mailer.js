7c32ad2e3199da3ea3b980c1aa7543181ec89704
Luca Aguzzoli <luca.aguzzoli@edu.unito.it>
2016-04-23 17:56:32 +0200

    Some improvements at the mailer function

--- js/mailer.js
+++ js/mailer.js
@@ -1,26 +1,26 @@
-function mailer(){
-	var nome = document.getElementById('form_name').value;	
-	var email = document.getElementById('form_email').value;	
-	var message = document.getElementById('form_message').value;
-	var http;
-    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-        http = new XMLHttpRequest();
-    }
-    else {// code for IE6, IE5
-        http = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    var url = "mailer.php";
-    var params = "name=" + nome+"&email="+email+"&message="+message;
-    http.open("POST", url, true);
-//Send the proper header information along with the request
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    http.setRequestHeader("Content-length", params.length);
-    http.setRequestHeader("Connection", "close");
-    http.onreadystatechange = function () {
-        if (http.readyState === 4 && http.status === 200) {
-        	alert("Message sended successfully");
-        }
-	alert("Impossible to send this message due to techical problem.");
-    };
-    http.send(params);
+function mailer() {
+   var nome = document.getElementById('form_name').value;
+   var email = document.getElementById('form_email').value;
+   var message = document.getElementById('form_message').value;
+   var http;
+   if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+      http = new XMLHttpRequest();
+   } else { // code for IE6, IE5
+      http = new ActiveXObject("Microsoft.XMLHTTP");
+   }
+   var url = "mailer.php";
+   var params = "name=" + nome + "&email=" + email + "&message=" + message;
+   http.open("POST", url, true);
+   //Send the proper header information along with the request
+   http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+   http.setRequestHeader("Content-length", params.length);
+   http.setRequestHeader("Connection", "close");
+   http.onreadystatechange = function() {
+      if (http.readyState === 4 && http.status === 200) {
+         if (http.responseText == "true")
+            console.log('Email inviata con successo')
+         else console.log('Invio email non riuscito')
+      }
+   };
+   http.send(params);
 }
--- mailer.php
+++ mailer.php
@@ -3,8 +3,20 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
+if(!validateEmail($email)){
+   echo "false";
+   return false;
+}
+
 $complete_message = "Messaggio inviato da " . $name . "\r\n" . "Messaggio: " . $message;
 
 $headers = "From: " . $email;
 
-echo mail("luca.aguzzoli@hotmail.it", "messaggio dal sito", $complete_message);
+mail("luca.aguzzoli@hotmail.it", "messaggio dal sito", $complete_message);
+
+echo "true";
+
+function vaidateEmail($email){
+   $validator = "/[a-zA-Z0-9!#$%&'*+-\/=?^_`{|}~.]+@[a-zA-Z0-9-.]+.[a-zA-Z]+/";
+   return preg_match($validator, $email);
+}
